f8e718ecb427fa0673bb825b24e77bf4e1c2883f
EircJin101 <foreveryouth93@163.com>
2023-07-05 14:30:10 +0800

    chore: 新增结果&代码格式

--- src/data_structure/hybrid_a_star_data_structure.h
+++ src/data_structure/hybrid_a_star_data_structure.h
@@ -3,8 +3,8 @@
 namespace eric {
 namespace hybrid_a_star {
 
-class Cost{
-private:
+class Cost {
+ private:
   PathPoint current_;
   PathPoint father_;
   double g_;  // real cost
@@ -12,7 +12,7 @@ private:
   double f_;  // final cost (f = g + h)
   std::vector<PathPoint> cost_path_;
 
-public:
+ public:
   void set_current(PathPoint& _t) {
     current_.set_utm_x(_t.utm_x());
     current_.set_utm_y(_t.utm_y());
@@ -23,48 +23,50 @@ public:
     father_.set_utm_y(_t.utm_y());
     father_.set_utm_theta(_t.utm_theta());
   }
-  PathPoint current() {return current_;}
-  PathPoint father() {return father_;}
+  PathPoint current() { return current_; }
+  PathPoint father() { return father_; }
 
-  void set_g(double t) {g_ = t;}
-  void set_h(double t) {h_ = t;}
-  void set_f(double t) {f_ = t;}
+  void set_g(double t) { g_ = t; }
+  void set_h(double t) { h_ = t; }
+  void set_f(double t) { f_ = t; }
 
-  double g() {return g_;}
-  double h() {return h_;}
-  double f() {return f_;}
+  double g() { return g_; }
+  double h() { return h_; }
+  double f() { return f_; }
 
   void set_cost_path(std::vector<PathPoint>& _t) {
-    for (PathPoint _p : _t){
+    for (PathPoint _p : _t) {
       cost_path_.emplace_back(_p);
     }
   }
-  std::vector<PathPoint> cost_path() { return cost_path_;}
+  std::vector<PathPoint> cost_path() { return cost_path_; }
 };
 
-class Node{
-public:
+class Node {
+ public:
   double x_index;
   double y_index;
   double yaw_index;
   double trailer_index;  // trailer angle
   bool direction;
   std::vector<PathPoint> path;  // path from parent -> current
-  // utm_x position [m], utm_y position [m], utm_theta angle [rad], trailer_angle [rad]
-  std::vector<bool> directions;  // directions of each points forward: true, backward:false
+  // utm_x position [m], utm_y position [m], utm_theta angle [rad],
+  // trailer_angle [rad]
+  std::vector<bool>
+      directions;  // directions of each points forward: true, backward:false
   double steer;  // steer input
   double cost;  // cost
   double parent_index;  // parent index
 };
 
-class NodeList{
-public:
+class NodeList {
+ public:
   double index = 0;
   Node node;
 };
 
-class Config{
-public:
+class Config {
+ public:
   double min_x;
   double min_y;
   double min_yaw;
@@ -81,42 +83,40 @@ public:
   double yaw_resolution;
 };
 
-class AStarNodeCost{
-public:
+class AStarNodeCost {
+ public:
   double node_x;
   double node_y;
   double cost;
   double parent_index;
 };
 
-class AStarNodeList{
-public:
+class AStarNodeList {
+ public:
   double index = 0;
   AStarNodeCost node;
 };
 
-class AStarOBMap{
-public:
+class AStarOBMap {
+ public:
   double x;
   double y;
   bool occupation;
 };
 
-class AStarMotion{
-public:
+class AStarMotion {
+ public:
   double x;
   double y;
   double dis;
 };
 
-class ReedsSheppPathsCost{
-public:
+class ReedsSheppPathsCost {
+ public:
   std::vector<port::Trajectory> rs_path_;
   std::vector<PathPoint> rs_path_pts_;
   double cost_;
 };
 
-
 }  // namespace hybrid_a_star
-}  // eric
-
+}  // namespace eric
